899a5666c83137ff91099837e7e64e271a8a5765
Amanda <88062177+amandahn@users.noreply.github.com>
2022-07-19 10:29:57 -0700

    Create draw.cpp

---
+++ DrawingLanguage/draw.cpp
@@ -0,0 +1,35 @@
+// draw.cpp
+// Written by Toby Donaldson for CMPT 125
+
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "draw.h"
+
+using namespace std;
+
+int main()
+{
+    sf::RenderWindow win(sf::VideoMode(WIDTH, HEIGHT));
+    win.setFramerateLimit(60);
+    bool drawn = false;
+
+    // main animation loop
+    while (win.isOpen()) {
+
+        sf::Event event;
+        while (win.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                win.close();
+            }
+        }
+
+        if (!drawn) {
+            win.clear(sf::Color::Black);
+            process_lines(win);
+            drawn = true;
+        }
+
+        win.display();
+    } 
+} // main
